bf16bb3a098ef489914abc8598fb9a149df5a740
2010m <mplich@student.agh.edu.pl>
2021-11-03 16:36:16 +0100

    finished zadanie_8

--- laboratorium_3/zadanie_8.py
+++ laboratorium_3/zadanie_8.py
@@ -1,8 +1,9 @@
 val1 = int(input("podaj wielkość choinki:"))
 
+print(' ' * (val1 - 1) + 'X')
 
-y=val1-1
-x=1
-for i in range(0,val1):
-    for i in range(0, x):
-        print('*', end="x")
+for i in range(1,val1):
+    print(' ' * (val1 - i - 1) + '*' * (i * 2 + 1))
+
+print(' ' * (val1 - 1) + 'U')
+
