559e5b644492d2a34a5e7df1a06982aca3699a51
2010m <mplich@student.agh.edu.pl>
2021-11-04 15:22:38 +0100

    finished zadanie_10

--- laboratorium_3/zadanie_10.py
+++ laboratorium_3/zadanie_10.py
@@ -1,33 +1,50 @@
+import random
+
 a = "T"
 print('Ten kalkulator obsługuje: dodawanie[+], odejmowanie[-], mnożenie[*], dzielenie[/], potęgowanie[**], pierwiastkowanie[^], losowanie[x]')
 print('Proszę wpisywać sam znak odpowiedni do danego działania')
 
+def dzialanie():
+    n = str(input('Podaj liczbę lub wylosuj [x]: '))
+    if n == 'x':
+        while n == 'x':
+            starting_point = float(input('Podaj początek zakresu: '))
+            ending_point = float(input('Podaj koniec zakresu: '))
+            if not starting_point > ending_point:
+                n = random.uniform(starting_point,ending_point)
+                print('Wylosowało: ',n)
+                return float(n)
+            else:
+                print('Zły zakres')
+    else:
+        return float(n)
+
+val1 = dzialanie()
+
 while a == "T":
-    operation = str(input('Co chcesz zrobić?'))
+    operation = str(input('Co chcesz zrobić? '))
+    val2 = dzialanie()
 
-    if operation == "+":
-        val1 = float(input("Wpisz pierwszą liczbę:"))
-        val2 = float(input("Wpisz drugą liczbę:"))
-        print("Wynik:",val1 + val2)
+    if operation == "+":    val1 = val1 + val2
 
-    elif operation == "-":
-        print(2)
+    elif operation == "-":  val1 = val1 - val2
 
-    elif operation == "*":
-        print(3)
+    elif operation == "*":  val1 = val1 * val2
 
     elif operation == "/":
-        print(4)
+        if val2 == 0:
+            print('Pamiętaj cholero')
+        else:
+            val1 = val1 / val2
 
-    elif operation == "**":
-        print(5)
+    elif operation == "**": val1 = val1 ** val2
 
-    elif operation == "^":
-        print(6)
+    elif operation == "^":  val1 = val1 ** (1/val2)
 
     else:
         print("zła operacja")
 
-    a = str(input('Czy chcesz wykonać kolejne obliczenie?[T]/[N]'))
+    print('Wynik ', val1)
+    a = str(input('Czy chcesz wykonać kolejne obliczenie?[T]/[N] '))
 
 print("Koniec obliczeń")
