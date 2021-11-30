840375ba75051aaff85a31ad53e721edba88d556
2010m <mplich@student.agh.edu.pl>
2021-11-30 18:06:52 +0100

    changed zadanie_6v2.py

--- laboratorium_7/zadanie_6v2.py
+++ laboratorium_7/zadanie_6v2.py
@@ -4,12 +4,12 @@
 #Można założyć, że dane są poprawne i w ciągu znajduje się wystarczająca liczba elementów.
 flag = True
 a = str("yes")
-list_substring = []
-final_list_sub=[]
-list_value = []
 check = True
 
 while a == "yes":
+    list_substring = []
+    final_list_sub = []
+    list_value = []
     while flag == True:
         try:
             fullcode = input("Write the message: ")
@@ -25,30 +25,40 @@ while a == "yes":
                 #print(substring)
                 list_substring.append(substring)
                 fullcode = fullcode[(substring_end + 1):]
-                #print(list_substring)
+                print(list_substring)
 
         except ValueError:
             flag = False
 
-    if list_substring == None:
-        print("No matches Found :(")
-        break
+    if list_substring == []:
+        print("No matches found :(")
 
-    for i in list_substring:
-        final_list_sub += i
-        print(final_list_sub)
+    else:
+        for i in list_substring:
+            final_list_sub += i
+            print(final_list_sub)
 
-    for i in final_list_sub:
-        value = ord(i)
-        print(i,value)
-        list_value.append(value)
+        if len(final_list_sub) >= 5:
+            for i in final_list_sub:
+                value = ord(i)
+                print(i,value)
+                list_value.append(value)
 
-    list_value.sort()
-    list_value.reverse()
-    print(list_value)
+            list_value.sort()
+            list_value.reverse()
+            print(list_value)
 
-    b = list_value[4]
-    result = chr(b)
-    print("Your result: ",result)
-    a = str(input("Wanna try again?:[yes/no] "))
+            b = list_value[4]
+            result = chr(b)
+            print("Your result: ",result)
+            a = str(input("Wanna try again?:[yes/no] "))
+        else:
+            print("Code too short >:(")
     flag = True
+
+#Przypadki testowe:
+# Dobra dana:       s0bcdefgh0es --> reasult = d
+# Wiele zer:        0ab0bc0cd0fgh0ij --> reasult = c
+# Brak zer:         abcdefg --> No matches found :(
+# Tylko jedno zero:             abc0ddd --> No matches found :(
+# Tekst pomiędzy zerami < 5:    abd0dc0fghi --> Code too short >:(
